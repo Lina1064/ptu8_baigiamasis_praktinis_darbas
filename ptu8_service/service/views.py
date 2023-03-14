934e7ba26e30190550b1f1808c25ae4cdee44621
Lina1064 <lina.vigricaite@gmail.com>
2023-03-14 17:06:48 +0200

    home page

--- ptu8_service/service/views.py
+++ ptu8_service/service/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . import models 
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Laba diena!')
+def home(request):
+    service_count = models.Service.objects.count()
+    customer_count = models.Customer.objects.count()
+    done_order_count = models.Order.objects.filter(status='d').count()
+
+    return render(request, 'service/home.html', {
+        'service_count': service_count,
+        'customer_count': customer_count,
+        'done_order_count': done_order_count,
+    })
