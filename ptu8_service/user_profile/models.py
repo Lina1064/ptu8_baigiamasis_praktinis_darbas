bc5d737d3e2b5c5b585459a92cc9f06949cbc85d
Lina1064 <lina.vigricaite@gmail.com>
2023-03-25 14:03:50 +0200

    user profile

--- ptu8_service/user_profile/models.py
+++ ptu8_service/user_profile/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from PIL import Image
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        get_user_model(), 
+        verbose_name=_("user"), 
+        on_delete=models.CASCADE,
+        related_name='profile',
+        unique=True,
+    )
+    photo = models.ImageField(
+        _("photo"), 
+        upload_to='user_profile/photos/',
+        default='user_profile/photos/default.png',
+    )
+    
+    def __str__(self) -> str:
+        return str(self.user)
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        photo = Image.open(self.photo.path)
+        if photo.height > 300 or photo.width > 300:
+            output_size = (300, 300)
+            photo.thumbnail(output_size)
+            photo.save(self.photo.path)
+            print("photo resized")
